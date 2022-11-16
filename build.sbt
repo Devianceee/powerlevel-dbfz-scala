073ff568813ada2d6457c0c87584e6bff9c47e19
Mohammed Shahidul Islam <pr3dator2193@gmail.com>
2022-11-16 11:31:35 +0000

    Cron job

--- build.sbt
+++ build.sbt
@@ -1,6 +1,19 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
+val http4sVersion = "0.23.10"
+
+libraryDependencies ++= Seq(
+  "eu.timepit" %% "fs2-cron-cron4s" % "0.7.2", //and/or
+  "eu.timepit" %% "fs2-cron-calev" % "0.7.2",
+  "org.typelevel" %% "cats-effect" % "3.4.0",
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  "org.http4s" %% "http4s-circe" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-client" % http4sVersion,
+  "io.circe" %% "circe-generic" % "0.14.0-M5"
+
+)
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
 package org.powerlevel
 
-object Main extends App {
-  println("Hello world!")
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import cron4s.Cron
+import eu.timepit.fs2cron.cron4s.Cron4sScheduler
+import fs2.Stream
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object Main extends IOApp.Simple {
+  val printTime: Stream[IO, Unit] = Stream.eval(IO(println("Hello world! The time is: " +  LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))))
+
+  override def run: IO[Unit] = {
+    val cronScheduler = Cron4sScheduler.systemDefault[IO]
+    val evenSeconds = Cron.unsafeParse("*/120 * * ? * *")
+    val scheduled = cronScheduler.awakeEvery(evenSeconds) >> printTime
+
+    IO(scheduled.attempt.compile.drain.unsafeRunSync())
+  }
 }
