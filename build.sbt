cf553459775d2566653353ef223380456988522f
Shahi <pr3dator2193@gmail.com>
2022-11-17 13:50:58 +0000

    Added MsgPack decoding from POST response from LOGIN API

--- build.sbt
+++ build.sbt
@@ -1,7 +1,8 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+ThisBuild / scalaVersion := "2.12.17"
 val http4sVersion = "0.23.10"
+resolvers += Resolver.jcenterRepo
 
 libraryDependencies ++= Seq(
   "eu.timepit" %% "fs2-cron-cron4s" % "0.7.2", //and/or
@@ -11,9 +12,14 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-circe" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
-  "io.circe" %% "circe-generic" % "0.14.0-M5",
   "org.http4s" %% "http4s-ember-server" % http4sVersion,
   "org.http4s" %% "http4s-ember-client" % http4sVersion,
+  "org.velvia" %% "msgpack4s" % "0.6.0",
+  "com.google.protobuf" % "protobuf-java-util" % "4.0.0-rc-2",
+  "org.json4s" %% "json4s-native" % "3.5.0",
+  "org.json4s" %% "json4s-jackson" % "3.5.0",
+  "io.circe" %% "circe-generic" % "0.14.3",
+  "io.circe" %% "circe-literal" % "0.14.3"
 )
 
 lazy val root = (project in file("."))
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,41 +5,54 @@ import cats.effect.unsafe.implicits.global
 import cron4s.Cron
 import eu.timepit.fs2cron.cron4s.Cron4sScheduler
 import fs2.Stream
-import io.circe.generic.auto._
-import org.http4s.Method.{GET, POST}
-import org.http4s.circe.jsonOf
+import org.http4s.EntityDecoder.byteArrayDecoder
+import org.http4s.Method.POST
 import org.http4s.client._
 import org.http4s.client.dsl.io._
-import org.http4s.headers.{Accept, Authorization}
 import org.http4s.implicits.http4sLiteralsSyntax
-import org.http4s.{AuthScheme, Credentials, EntityDecoder, MediaType, UrlForm}
+import org.http4s.{EntityDecoder, UrlForm}
+import org.velvia.MsgPack
+import org.velvia.msgpack.RawStringCodecs.StringCodec
+import org.velvia.msgpack._
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-case class AuthResponse(access_token: String)
+import javax.xml.bind.DatatypeConverter
 
 object Main extends IOApp.Simple {
-  implicit val authResponseEntityDecoder: EntityDecoder[IO, AuthResponse] = jsonOf
-
-  val printTime: Stream[IO, Unit] = Stream.eval(IO(println("Hello world! The time is: " +  LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))))
-  val statement: Stream[IO, Unit] = Stream.eval(IO(if (56 > 0) println(100) else println(0)))
   val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
-//  val example =
-  val postRequest = POST(
+  val printTime: Stream[IO, Unit] = Stream.eval(IO(println("Hello world! The time is: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))))
+  val statement: Stream[IO, Unit] = Stream.eval(IO(if (56 > 0) println(100) else println(0)))
+  val cronScheduler = Cron4sScheduler.systemDefault[IO]
+  val everyMinute = Cron.unsafeParse("1 * * ? * *")
+  val every20Secs = Cron.unsafeParse("*/20 * * ? * *")
+  val tasks = cronScheduler.schedule(List(every20Secs -> printTime, every20Secs -> statement))
+
+  def convertBytesToHex(bytes: Array[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+      sb.append(String.format("%02x", Byte.box(b)))
+    }
+    sb.toString
+  }
+
+  //    val loginJson = parse("""["", "", 2,"0.0.3", 3],["76561198077238939", "110000106f8de9b", 256, 0]""")
+
+  val loginRequest = POST(
     UrlForm(
-      "data" -> "9295b2313830323035303733333032393434363233ad3633356661306433313734303802a5302e302e3303950701000a961cffff01ff90"
+      "data" -> "dd00000002dd00000005a0a002a5302e302e3303dd00000004b13736353631313938303737323338393339af313130303030313036663864653962cd010000"
     ),
-    uri"https://dbf.channel.or.jp/api/catalog/get_replay"
+    uri"https://dbf.channel.or.jp/api/user/login"
   )
 
   override def run: IO[Unit] = {
-    val cronScheduler = Cron4sScheduler.systemDefault[IO]
-//    val everyMinute = Cron.unsafeParse("1 * * ? * *")
-    val every20Secs = Cron.unsafeParse("*/20 * * ? * *")
-//    val tasks = cronScheduler.schedule(List(everyMinute -> printTime, everyMinute -> statement))
-    val tasks = cronScheduler.schedule(List(every20Secs -> printTime, every20Secs -> statement))
-    println(httpClient.expect[String](postRequest).unsafeRunSync())
+
+    val response = httpClient.expect[Array[Byte]](loginRequest).unsafeRunSync()
+    val unpacked = MsgPack.unpack(response).toString
+    println(unpacked)
+
+    val test = pack("test")
+    println(unpack[String](DatatypeConverter.parseHexBinary(convertBytesToHex(test))))
     IO.unit
-//    IO(println(example.unsafeRunSync()))
   }
 }
