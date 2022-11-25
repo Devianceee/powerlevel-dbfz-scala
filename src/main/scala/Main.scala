9c2b3e92c3909e2b48b018dcba7ea11d1d410b64
Shahi <pr3dator2193@gmail.com>
2022-11-25 21:08:43 +0000

    - Parallel computing with server + requests - Removed cron job as I should just ping server again as soon as I'm done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,9 @@ package org.powerlevel
 import cats.effect.IO.executionContext
 import cats.effect.unsafe.implicits.global
 import cats.effect.{IO, _}
+import cats.implicits.catsSyntaxTuple2Parallel
 
 import scala.concurrent.ExecutionContext
-
 import com.comcast.ip4s._
 import cron4s.Cron
 import eu.timepit.fs2cron.cron4s.Cron4sScheduler
@@ -19,6 +19,8 @@ import org.http4s.client.{Client, JavaNetClientBuilder}
 import org.http4s.ember.server._
 import org.http4s.implicits._
 import org.http4s.server.{Router, Server}
+
+import scala.concurrent.duration.DurationInt
 //import org.powerlevel.Main.loginTimestamp
 import cats.effect._
 import org.http4s._
@@ -30,10 +32,7 @@ object Main extends IOApp.Simple {
 //  val login_url = "https://dbf.channel.or.jp/api/user/login"
 
   val numberOfMatchesQueried = 1000 // better to do this via .conf file or some other environment way
-
-  val printForReplay: Stream[IO, Unit] = Stream.eval(IO(println("Replay ping! The time is: " + Utils.timeNow)))
-
-  def replayResponseRank: Stream[IO, Any] = Stream.eval(IO(Database.writeToDB(for {
+  def replayResponseRank: IO[List[Any]] = Database.writeToDB(for {
     r1 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 11))
     r2 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 501))
     r3 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 1001))
@@ -45,26 +44,30 @@ object Main extends IOApp.Simple {
     r9 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 4001))
     r10 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 4501))
     r11 <- Utils.parseReplays(Requests.replayRequest(0, numberOfMatchesQueried, 5001))
-  } yield r1 ++ r2 ++ r3 ++ r4 ++ r5 ++ r6 ++ r7 ++ r8 ++ r9 ++ r10 ++ r11).unsafeRunSync()))
+  } yield r1 ++ r2 ++ r3 ++ r4 ++ r5 ++ r6 ++ r7 ++ r8 ++ r9 ++ r10 ++ r11)
 
-  val cronScheduler = Cron4sScheduler.systemDefault[IO]
-  val every20Secs = Cron.unsafeParse("5,25,45 * * ? * *")
-  val every15Mins = Cron.unsafeParse("0 0,15,30,45 * ? * *")
-
-  val cronTasks = cronScheduler.schedule(List(
-    every20Secs -> printForReplay,
-    every20Secs -> replayResponseRank,
-  ))
+  lazy val loopRequest: IO[Any] = replayResponseRank >> IO.println(Thread.currentThread().getName + " - Request Finished!") >> IO.sleep(2.seconds) >> loopRequest
 
   println("Starting PowerLevel.info \nBy Deviance#3806\n\n")
 
-  def run: IO[Unit] = {
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }.orNotFound
+
+  val server: IO[Unit] = EmberServerBuilder
+    .default[IO]
+    .withHost(ipv4"0.0.0.0")
+    .withPort(port"8000")
+    .withHttpApp(helloWorldService)
+    .build
+    .use(_ => IO.never)
+    .as(IO.unit)
+
+  override def run: IO[Unit] = {
     // TODO: run server which can be pinged to be able to cancel scheduled task and gracefully close database in case of maintenance
     // TODO: can probably add more flatMaps to places for better comprehension / less nesting (removes inner IO)
     // TODO: traverse keyword is very nice, see if I can use it in other places
-
-    cronTasks.repeat.compile.drain.unsafeRunSync() // doesnt run without unsafeRunSync() why??
-//    Database.writeToDB(Utils.parseReplays(Requests.replayRequest(loginTimestamp, 0, 10, 11))).unsafeRunSync()
-    IO.unit
+    (loopRequest, server).parMapN { (_, _) => ()}
   }
 }
